6b10812b9437c8798595999326cbed91ccf48038
sajibahmedb98 <mmaa6560@gmail.com>
2023-09-09 22:45:34 +0600

    user db setup

---
+++ db/db_setup.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://sa@localhost:5432/fast_api_lms"
+
+# create engine
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+#DB Utilities
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
