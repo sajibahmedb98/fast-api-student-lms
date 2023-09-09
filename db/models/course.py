8e600aa2d25411f0fb0c46f233e394fe9d36fd82
sajibahmedb98 <mmaa6560@gmail.com>
2023-09-10 00:06:23 +0600

    courses and user db setub

--- db/models/course.py
+++ db/models/course.py
@@ -1,33 +1,95 @@
 import enum
 
-from sqlalchemy import Column, Integer, String, ForeignKey, Enum, Text, Boolean
+from sqlalchemy import Enum, Column, ForeignKey, Integer, String, Text, Boolean
 from sqlalchemy.orm import relationship
+from sqlalchemy_utils import URLType
 
 from ..db_setup import Base
+from .user import User
+from .mixins import Timestamp
 
 
-class Role(enum.Enum):
-    teacher = 1
-    student = 2
+class ContentType(enum.Enum):
+    lesson = 1
+    quiz = 2
+    assignment = 3
 
 
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(100), unique=True, index=True, nullable=False)
-    role = Column(Enum(Role))
-    
-    profile = relationship("Profile", back_populates="owner", uselist=False)
-
-
-class Profile(Base):
-    __tablename__ = "profiles"
+class Course(Timestamp, Base):
+    __tablename__ = "courses"
 
     id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String(50), nullable=False)
-    bio = Column(Text, nullable=True)
-    is_active = Column(Boolean, default=False)
+    title = Column(String(200), nullable=False)
+    description = Column(Text, nullable=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    
-    owner = relationship("User", back_populates="profile")
+
+    created_by = relationship(User)
+    sections = relationship("Section", back_populates="course", uselist=False)
+    student_courses = relationship("StudentCourse", back_populates="course")
+
+
+class Section(Timestamp, Base):
+    __tablename__ = "sections"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(200), nullable=False)
+    description = Column(Text, nullable=True)
+    course_id = Column(Integer, ForeignKey("courses.id"), nullable=False)
+
+    course = relationship("Course", back_populates="sections")
+    content_blocks = relationship("ContentBlock", back_populates="section")
+
+
+class ContentBlock(Timestamp, Base):
+    __tablename__ = "content_blocks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(200), nullable=False)
+    description = Column(Text, nullable=True)
+    type = Column(Enum(ContentType))
+    url = Column(URLType, nullable=True)
+    content = Column(Text, nullable=True)
+    section_id = Column(Integer, ForeignKey("sections.id"), nullable=False)
+
+    section = relationship("Section", back_populates="content_blocks")
+    completed_content_blocks = relationship(
+        "CompletedContentBlock", back_populates="content_block"
+    )
+
+
+class StudentCourse(Timestamp, Base):
+    """
+    Students can be assigned to courses.
+    """
+
+    __tablename__ = "student_courses"
+
+    id = Column(Integer, primary_key=True, index=True)
+    student_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    course_id = Column(Integer, ForeignKey("courses.id"), nullable=False)
+    completed = Column(Boolean, default=False)
+
+    student = relationship(User, back_populates="student_courses")
+    course = relationship("Course", back_populates="student_courses")
+
+
+class CompletedContentBlock(Timestamp, Base):
+    """
+    This shows when a student has completed a content block.
+    """
+
+    __tablename__ = "completed_content_blocks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    student_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    content_block_id = Column(
+        Integer, ForeignKey("content_blocks.id"), nullable=False
+    )
+    url = Column(URLType, nullable=True)
+    feedback = Column(Text, nullable=True)
+    grade = Column(Integer, default=0)
+
+    student = relationship(User, back_populates="student_content_blocks")
+    content_block = relationship(
+        ContentBlock, back_populates="completed_content_blocks"
+    )
--- db/models/user.py
+++ db/models/user.py
@@ -1,33 +1,39 @@
 import enum
 
-from sqlalchemy import Column, Integer, String, ForeignKey, Enum, Text, Boolean
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Enum, Text
 from sqlalchemy.orm import relationship
 
 from ..db_setup import Base
+from .mixins import Timestamp
 
 
-class Role(enum.Enum):
+class Role(enum.IntEnum):
     teacher = 1
     student = 2
 
 
-class User(Base):
+class User(Timestamp, Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(100), unique=True, index=True, nullable=False)
     role = Column(Enum(Role))
-    
+    is_active = Column(Boolean, default=False)
+
     profile = relationship("Profile", back_populates="owner", uselist=False)
+    student_courses = relationship("StudentCourse", back_populates="student")
+    student_content_blocks = relationship(
+        "CompletedContentBlock", back_populates="student"
+    )
 
 
-class Profile(Base):
+class Profile(Timestamp, Base):
     __tablename__ = "profiles"
 
     id = Column(Integer, primary_key=True, index=True)
     first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
     bio = Column(Text, nullable=True)
-    is_active = Column(Boolean, default=False)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    
+
     owner = relationship("User", back_populates="profile")
